9ad96a8a01b4675203b8e2a40f8ab6b308beb0e4
Navneet Shrivastava <navneet.shrivastava@precisely.com>
2022-06-28 16:40:02 +0530

    Intial check in.-- Created the project

---
+++ atm-api/src/main/java/com/navneet/atm/atmapi/AtmApiApplication.java
@@ -0,0 +1,13 @@
+package com.navneet.atm.atmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmApiApplication.class, args);
+	}
+
+}
---
+++ atm-api/src/test/java/com/navneet/atm/atmapi/AtmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navneet.atm.atmapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
