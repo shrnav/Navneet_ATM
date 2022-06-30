c5a4c4be199eb9a618dfca22f4510627c4a99ff5
Navneet.Shrivastava <navneet.shrivastava@precisely.com>
2022-06-30 19:57:21 +0530

    Added common constants class

---
+++ atm-api/src/main/java/com/navneet/atm/atmapi/util/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.navneet.atm.atmapi.util;
+
+public class CommonConstants {
+    public static final String RETRY_PIN = "You have entered invalid pin. Remaining Attempts: ";
+    public static final String AVAILABLE_BALANCE = "Your available balance is: ";
+
+    public static final String RETRY_PIN_EXCEPTION = "You have entered invalid PIN three time. Your account has been blocked for the day!!!";
+}
